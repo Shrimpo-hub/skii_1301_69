faa2382bb327c2d87d25cb7bb37ad254dc7c3ed2
Shrimpo-hub <mapraserik@gmail.com>
2026-08-27 17:39:58 +0700

    ah_commit

---
+++ Assets/Scipts/tree.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+
+    private MeshRenderer rd;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rd = GetComponent<MeshRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rd.material.color = Color.red;
+        Player player = collision.gameObject.GetComponent<Player>();
+
+        if (player == null)
+            return;
+
+        player.HP -= 15;
+        UIManager.instance.ShowNotiText($"Hurt -15\nHP: {player.HP}");
+
+        if (player.HP <= 0)
+        {
+            player.HP = 0;
+            UIManager.instance.ShowNotiText($"You Are DEAD?\n Points: {player.Point}");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        rd.material.color = new Color32(109, 58, 22, 255);
+    }
+}
